49b48c23095e6936cf43d215d3da58206f70a9db
Douglas <douglazdv@gmail.com>
2026-09-13 22:51:11 -0300

    refatoração de formatação

--- src/supermercado/Supermercado.java
+++ src/supermercado/Supermercado.java
@@ -7,17 +7,16 @@ public static void main(String[] args) {
 
 // carrinho
 CarrinhoDeCompras c = new CarrinhoDeCompras();
-    c.adicionarProduto("Arroz", 1, 3.99);
-    c.adicionarProduto("Filé de frango sassami", 2, 21.99);
-
+c.adicionarProduto("Arroz", 1, 3.99);
+c.adicionarProduto("Filé de frango sassami", 2, 21.99);
 c.calcularTotal();
 
 
 //pedido
 
 Pedido p = new Pedido();
-            p.setNumeroPedido(1);
-            p.setCarrinho(c);
+p.setNumeroPedido(1);
+p.setCarrinho(c);
 
 p.fecharPedido();
 
