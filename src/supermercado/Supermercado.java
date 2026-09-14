bb37f244aa7d831608a844dfb89d1621cf20d2bf
Douglas <douglazdv@gmail.com>
2026-09-13 23:05:40 -0300

    refatoração de nomeação

--- src/supermercado/Supermercado.java
+++ src/supermercado/Supermercado.java
@@ -2,23 +2,23 @@ package supermercado;
 
 public class Supermercado {
 
-// teste
+
 public static void main(String[] args) {
 
-// carrinho
-CarrinhoDeCompras c = new CarrinhoDeCompras();
-c.adicionarProduto("Arroz", 1, 3.99);
-c.adicionarProduto("Filé de frango sassami", 2, 21.99);
-c.calcularTotal();
+
+CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+carrinho.adicionarProduto("Arroz", 1, 3.99);
+carrinho.adicionarProduto("Filé de frango sassami", 2, 21.99);
+carrinho.calcularTotal();
 
 
-//pedido
 
-Pedido p = new Pedido();
-p.setNumeroPedido(1);
-p.setCarrinho(c);
 
-p.fecharPedido();
+Pedido pedido = new Pedido();
+pedido.setNumeroPedido(1);
+pedido.setCarrinho(c);
+
+pedido.fecharPedido();
 
 }
 
